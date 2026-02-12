de0f933a07bb74cc624cb030d2ed4b53c97bcc2f
Ayush Joshi <ayushjoshi692@gmail.com>
2026-02-13 03:15:55 +0530

    Fix CORS URL and credentials

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,12 +18,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://https://farm-lence.vercel.app"
+        "https://farm-lence.vercel.app"
     ],
-    allow_credentials=True,
+    allow_credentials=False,   # 👈 change this
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 
